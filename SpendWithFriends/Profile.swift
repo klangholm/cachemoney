f12dcca842c0e7aa98032dae3db1c80090d77f65
Leoul Yiheyis <leoul16@vt.edu>
2017-06-07 17:34:54 -0400

    added model group and profile class

---
+++ SpendWithFriends/Profile.swift
@@ -0,0 +1,44 @@
+//
+//  Profile.swift
+//  HackathonApp
+//
+//  Created by Yiheyis, Leoul on 6/7/17.
+//  Copyright © 2017 klangholm. All rights reserved.
+//
+
+import UIKit
+
+
+class Profile : NSObject {
+    
+    fileprivate var name: String
+    fileprivate var id: String
+    fileprivate var pic: UIImage?
+    
+    
+    fileprivate var runningTabs = [Tab]()  //Array<Tab>?
+    
+    init(name: String, id: String) {
+        self.name = name
+        self.id = id
+    }
+    
+    
+    func setName(newName: String) {
+        self.name = newName
+    }
+    
+    var getName: String {
+        return self.name
+    }
+    
+    func setProfilePic(pic: UIImage) {
+        self.pic = pic
+    }
+    
+    var getPic: UIImage {
+        return self.pic!
+    }
+    
+    
+}
